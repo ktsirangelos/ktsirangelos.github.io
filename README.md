9bf076b163f5370e6e022814a1df5efb9a3f0327
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-07-31 10:35:53 +0200

    update README

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-[makeTree() <- that one](https://github.com/ktsirangelos/ktsirangelos.github.io/blob/caefdc04c0eff8fef495677d54c6b9b75e461506/js/scripts.js#L7)
+This repository contains a function which converts JavaScript objects to tree-like strings.
+
+For example, this object
+
+```javascript
+example = {
+  "a": {
+    "b": {
+      "c": null
+      "d": {
+        "e": null
+      },
+    },
+    "f": null
+  }
+};
+```
+
+is converted to this tree
+
+```
+a
+├── b
+│   ├── c
+│   └── d
+│       └── e
+└── f
+```
+
+Notice that the initial object needs to be in a particular format: every value (of a [key-value pair](https://javascript.info/object)) should be either an object or `null`.\
+That's because in order to make the tree, we're calling the function recursively and `null` is the condition that stops the recursion.
+
+[See how it works](https://github.com/ktsirangelos/ktsirangelos.github.io/blob/caefdc04c0eff8fef495677d54c6b9b75e461506/js/scripts.js#L7)
 
--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,34 @@
 // Imports
 import {header, about, projects, stackOne, stackTwo, footer } from "./data.js";
 
-// Functions
-const makeTree = function(obj, indent = '', isRoot = true) {
-  let tree = '';
-  const keys = Object.keys(obj);
+// objectToTree 
+const objectToTree = function(obj, indent = '', isRoot = true) { 
+
+  let tree = ''; 
+  const keys = Object.keys(obj); 
+
+  // Iterates over each key in the object using forEach loop.
   keys.forEach((key, index) => {
+    // Checks if the current key is the last in the keys array.
     const isLast = index === keys.length - 1;
+
+    // Adds to the tree string based on whether the node is root and/or the last child.
     tree += `${indent}${isRoot ? '' : (isLast ? '└── ' : '├── ')}${key}\n`;
+
+    // Checks if the value of the current key is an object and not null.
     if (typeof obj[key] === 'object' && obj[key] !== null) {
+      // Sets the childPrefix based on whether the current key is the last in the array.
       let childPrefix = isLast ? '    ' : '│   ';
+
+      // Constructs the indentation for the next level, taking into account whether it is the root.
       let newIndent = isRoot ? indent : indent + childPrefix;
-      tree += makeTree(obj[key], newIndent, false);
+
+      // Recursively calls the objectToTree function to process the children of the current key, adding the result to the tree string.
+      tree += objectToTree(obj[key], newIndent, false);
     }
   });
-  return tree;
+
+  return tree; 
 }
 
 const changeText = function() {
@@ -29,8 +43,8 @@ const changeText = function() {
 // Init
 document.querySelector('header').innerHTML = `${header}`;
 document.querySelector('.about').innerHTML = `<p>${about}</p>`;
-document.querySelector('.projects').innerHTML = `<pre>${makeTree(projects)}</pre>`;
-document.querySelector('.stack-1').innerHTML = `<pre>${makeTree(stackOne)}</pre>`;
-document.querySelector('.stack-2').innerHTML = `<pre>${makeTree(stackTwo)}</pre>`;
+document.querySelector('.projects').innerHTML = `<pre>${objectToTree(projects)}</pre>`;
+document.querySelector('.stack-1').innerHTML = `<pre>${objectToTree(stackOne)}</pre>`;
+document.querySelector('.stack-2').innerHTML = `<pre>${objectToTree(stackTwo)}</pre>`;
 document.querySelector('footer').innerHTML = `<pre>${footer}</pre>`;
 document.querySelector('#this').addEventListener('click', changeText);

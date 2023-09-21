e93c7e30d1aa6bb569cab0dd0b5bed1c0d5ca194
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-09-21 12:20:12 +0200

    remove projects

--- devfiles/scripts/data.js
+++ devfiles/scripts/data.js
@@ -12,101 +12,105 @@ export const header = `
   <a href="https://github.com/ktsirangelos" target="_blank" rel="noopener noreferrer">GitHub</a>
 </div>
 <button id="theme-toggle" class="theme"></button>
-`
+`;
 
 export const about = `
 Hey, I am a web developer based in Rotterdam, NL. I enjoy creating robust, minimal interfaces that get the job done effectively and
 efficiently while providing optimal user experience. I deeply care about making things green, sustainable, educational and fair to all
 groups and individuals. For any questions or work you would like to discuss, feel free to write me an email.
-`
-export const projects = {
-  "<span class='tree-title'>Projects</span>": {
-    "Static": {
-      "<a id='this' style='cursor: pointer;'>this website →</a>": {
-        "<a href='https://github.com/ktsirangelos/ktsirangelos.github.io' target='_blank' rel='noopener noreferrer'>see code →</a>": null
-      },
-      "<a href='https://ktsirangelos.github.io/fivequarters.studio' target='_blank' rel='noopener noreferrer'>five quarters →</a>": {
-        "<a href='https://github.com/ktsirangelos/fivequarters.studio' target='_blank' rel='noopener noreferrer'>see code →</a>": null
-      },
-      "<a href='https://ktsirangelos.github.io/dida.studio/' target='_blank' rel='noopener noreferrer'>dida studio →</a>": {
-        "<a href='https://github.com/ktsirangelos/dida.studio' target='_blank' rel='noopener noreferrer'>see code →</a>": null
-      },
-      "<span class='in-progress'>in progress</span>": {
-        "<span class='in-progress'>...</span>": null
-      }
-    },
-    "Web Apps": {
-      "<a href='https://fragrant-mountain-6653.fly.dev/' target='_blank' rel='noopener noreferrer'>phonebook →</a>": {
-        "<a href='https://github.com/ktsirangelos/fso-part3' target='_blank' rel='noopener noreferrer'>see code →</a>": null
-      },
-      "<span class='in-progress'>in progress</span>": {
-        "<span class='in-progress'>...</span>": null
-      }
-    }
-  }
-};
+`;
+
+// export const projects = {
+//   "<span class='tree-title'>Projects</span>": {
+//     Static: {
+//       "<a id='this' style='cursor: pointer;'>this website →</a>": {
+//         "<a href='https://github.com/ktsirangelos/ktsirangelos.github.io' target='_blank' rel='noopener noreferrer'>see code →</a>":
+//           null,
+//       },
+//     },
+//     "Web Apps": {
+//       "<a href='https://fragrant-mountain-6653.fly.dev/' target='_blank' rel='noopener noreferrer'>phonebook →</a>":
+//       {
+//         "<a href='https://github.com/ktsirangelos/fso-part3' target='_blank' rel='noopener noreferrer'>see code →</a>":
+//           null,
+//       },
+//     },
+//   },
+// };
 
 export const stackOne = {
-  "<span class='tree-title'>Stack 1/2</span>": {
-    "Languages": {
-      "HTML5": null,
-      "CSS3": {
-        "Sass": null
+  "<span class='tree-title'>Stack 1/3</span>": {
+    Languages: {
+      HTML5: null,
+      CSS3: {
+        Sass: null,
       },
-      "Javascript": null,
-      "Typescript": null,
-      "<span class='in-progress'>GraphQL</span>": null
+      Javascript: null,
+      Typescript: null,
+      "<span class='in-progress'>GraphQL</span>": null,
     },
-    "Frameworks": {
-      "React": {
-        "<span class='in-progress'>Redux</span>": null
+    Frameworks: {
+      React: {
+        "React Router": null,
+        "<span class='in-progress'>Redux</span>": null,
       },
       "<span class='in-progress'>React Native</span>": null,
       "Node.js": {
-        "Express": null,
-        "EJS": null
-      }
+        Express: null,
+        EJS: null,
+      },
+      "<span class='in-progress'>Cypress</span>": null,
+      "Jest": null,
     },
-    "Data Stores": {
-      "<span class='in-progress'>Postgres</span>": {
-        "<span class='in-progress'>Sequelize</span>": null
-      },
-      "MongoDB": {
-        "Mongoose": null
-      },
-      "<span class='in-progress'>Redis</span>": null
-    }
-  }
+  },
 };
 
 export const stackTwo = {
-  "<span class='tree-title'>Stack 2/2</span>": {
-    "Processes": {
-      "<span class='in-progress'>CI/CD</span>": null,
-      "Testing": null,
-      "<span class='in-progress'>TDD</span>": null,
-      "Git": {
-        "GitLab": null,
-        "GitHub": null
-      }
+  "<span class='tree-title'>Stack 2/3</span>": {
+    "Data Stores": {
+      "<span class='in-progress'>Postgres</span>": {
+        "<span class='in-progress'>Sequelize</span>": null,
+      },
+      MongoDB: {
+        Mongoose: null,
+      },
+      "<span class='in-progress'>Redis</span>": null,
     },
-    "Tools": {
-      "CLI": {
-        "Linux Commands": null
+    Processes: {
+      "<span class='in-progress'>Agile</span>": {
+        "<span class='in-progress'>Scrum</span>": null,
       },
-      "Neovim": null,
-      "ESLint": null,
+      Testing: null,
+      "<span class='in-progress'>CI/CD</span>": null,
+      "<span class='in-progress'>TDD</span>": null,
+      Git: {
+        "<span class='in-progress'>Gitflow</span>": null,
+        GitLab: null,
+        GitHub: null,
+      },
+    },
+  },
+};
+
+export const stackThree = {
+  "<span class='tree-title'>Stack 3/3</span>": {
+    Tools: {
+      CLI: {
+        "Linux Commands": null,
+      },
+      Neovim: null,
+      ESLint: null,
+      Prettier: null,
       "<span class='in-progress'>Webpack</span>": null,
-      "Parcel": null,
-      "<span class='in-progress'>Cypress</span>": null,
-      "Jest": null,
+      Parcel: null,
+      Vite: null,
       "<span class='in-progress'>Docker</span>": {
-        "<span class='in-progress'>Composer</span>": null
+        "<span class='in-progress'>Composer</span>": null,
       },
-      "ChatGPT": null,
-      "Copilot": null,
-      "yarn": null,
-      "npm": null
-    }
-  }
+      ChatGPT: null,
+      Copilot: null,
+      yarn: null,
+      npm: null,
+    },
+  },
 };

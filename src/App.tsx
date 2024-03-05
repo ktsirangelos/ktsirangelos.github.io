8664a02b95775791839e823a19ea109d72d20f87
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-03-05 17:19:01 +0200

    Move files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import { Header } from "./Header";
-import { Main } from "./Main";
-import { Footer } from "./Footer";
+import { Header } from "./header/Header";
+import { Main } from "./main/Main";
+import { Footer } from "./footer/Footer";
 
 export const App = () => {
   return (
